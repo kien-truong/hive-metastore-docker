cddf0fd109f1c5021dff379ca851ab974e3fd87e
Kien Truong <duckientruong@gmail.com>
2021-11-24 18:19:11 +0700

    Use exec in entrypoint scripts

--- Dockerfile
+++ Dockerfile
@@ -14,6 +14,7 @@ RUN apt-get update && \
     apt-get clean && \
     apt-get autoremove
 
+# Entrypoint
 ENTRYPOINT ["/app/tini", "--"]
 CMD ["/app/docker-entrypoint.sh"]
 
--- files/docker-entrypoint.sh
+++ files/docker-entrypoint.sh
@@ -5,7 +5,7 @@ set -x
 cat /app/templates/hive-site.xml.tmpl | envsubst > ${HIVE_CONF}/hive-site.xml
 
 if [ "$INSTALL_MODE" == "true" ]; then
-    "${HIVE_HOME}/bin/schematool" -dbType mysql -initSchema
+    exec "${HIVE_HOME}/bin/schematool" -dbType mysql -initSchema
 else
-    "${HIVE_HOME}/bin/hive" --service metastore
+    exec "${HIVE_HOME}/bin/hive" --service metastore
 fi
